03f9282fdf0692e29906be1844697d278b919cc1
Neonhh <thenjhu@gmail.com>
2024-11-04 21:23:31 -0400

    respuesta 1 y readme

--- pregunta1/pregunta1.hs
+++ pregunta1/pregunta1.hs
@@ -20,14 +20,27 @@ conteo = conteoCola 0
 -- Primero definimos la funcion merge, que toma dos listas ordenadas y las une manteniendo el orden
 -- La defino como recursion de cola porque soy fan d optimizar la pila (?)
 -- Obtenemos el merge al tomar [] como primer argumento
-mergeCola :: [Integer] -> [Integer] -> [Integer] -> [Integer]
+-- Podria limitarme a enteros, pero realmente basta que sea un tipo que se pueda ordenar.
+mergeCola :: Ord a => [a] -> [a] -> [a] -> [a]
 -- Como Haskell es declarativo, debemos implementarlo como una recursion.
 -- Hacemos recursion de cola: Lo que hemos calculado de la lista se guarda en p.
 -- Caso base:
 mergeCola p a [] = p ++ a -- Como a y b estan ordenados, si alguno de ellos es vacio, entonces el resultado estara ordenado
 mergeCola p [] b = p ++ b
 -- Caso recursivo:  Hacemos la llamada dependiendo de los valores de los heads de las listas
-mergeCola p (as:a) (bs:b) = case compare as bs of  --Conservamos el head que sea menor, y seguimos la recursion con el resto
-                                LT -> seq (p ++ [as]) $ mergeCola  (p ++ [as]) a (bs:b)
-                                GT -> seq (p ++ [bs]) $ mergeCola (p ++ [bs]) (as:a) b
-                                EQ -> seq (p ++ [as]) $ mergeCola (p ++[as])a b -- este caso nos permite evitar duplicados
+mergeCola p (as:a) (bs:b) = if as <= bs then seq (p ++ [as]) $ mergeCola (p ++ [as]) a (bs:b)
+                            else seq (p ++ [bs]) $ mergeCola (p ++ [bs]) (as:a) b
+                    --Conservamos el head que sea menor, y seguimos la recursion con el resto
+
+--mergeSort
+mergeSort :: Ord a => [a] -> [a]
+-- Casos base
+mergeSort [] = []
+mergeSort [x] = [x]
+
+-- Caso recursivo
+-- take: toma los primeros n elementos de una lista
+-- drop: toma todos los elementos de una lista excepto los primeros n
+-- Hacemos las llamadas recursivas a mergeSort para asegurarnos de que los pedazos queden ordenados
+mergeSort a = mergeCola [] (mergeSort (take mitad a)) (mergeSort (drop mitad a))
+    where mitad = div (length a) 2
---
+++ pregunta1/readme.md
@@ -0,0 +1,13 @@
+# Respuesta a la pregunta 1
+
+En el archivo `pregunta1.hs` se encuentran las dos funciones: count(n), (llamada `conteo` porque count parece nombre de funcion predeterminada),
+y `mergeSort`.
+
+## Ejecutando los programas
+
+Primero se carga el archivo en ghci, usando `ghci pregunta1.hs`
+Luego, dentro de ghci:
+
+- Para el programa count, se llama directamente `conteo n`, siendo n el numero para el cual se desea hacer el calculo
+
+- Para Mergesort, similarmente se llama `mergeSort x`, donde x es el arreglo de enteros que se quiere ordenar.
