1b63d46676fe57176001f04d98f7fe5324691e13
Neonhh <thenjhu@gmail.com>
2024-11-04 20:26:15 -0400

    merge implementado

--- pregunta1/count.hs
+++ pregunta1/count.hs
@@ -1,6 +1,8 @@
---Ejercicio b de la parte ii de la pregunta ii
 --Nombre: Nestor Herrera
 --Carnet: 18-10796
+
+
+--Ejercicio b de la parte i de la pregunta 1--
 
 -- definimos primero una recursion de cola que realizara el calculo requerido cuando m=0
 conteoCola :: Integer -> Integer -> Integer
@@ -12,3 +14,16 @@ conteoCola m n = case mod n 2 of -- La funcion seq evalua el primer argumento, y
 -- 
 conteo :: Integer -> Integer
 conteo = conteoCola 0
+
+-- Ejercicio ii de la pregunta 1: mergesort
+
+-- Primero definimos la funcion merge, que toma dos listas ordenadas y las une manteniendo el orden
+merge :: [Integer] -> [Integer] -> [Integer]
+-- Como Haskell es declarativo, debemos implementarlo como una recursion, Casos base:
+merge a [] = a -- Como a y b estan ordenados, si alguno de ellos es vacio, entonces el resultado estara ordenado
+merge [] b = b
+-- Caso recursivo:  Hacemos la llamada dependiendo de los valores de los heads de las listas
+merge (as:a) (bs:b) = case compare as bs of  --Conservamos el head que sea menor, y seguimos la recursion con el resto
+                                LT -> as : merge a (bs:b)
+                                GT -> bs : merge (as:a) b
+                                EQ -> as : merge a b
